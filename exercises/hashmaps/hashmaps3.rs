5d1c9ebf604de3c1f146408fde7b7a30d65d9b53
YYmicro <YYmicro1044566658@outlook.com>
2024-04-25 08:58:50 +0800

    :construction: feat: hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -39,6 +39,20 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // if !scores.contains_key(&team_1_name){
+        //     scores.entry(team_1_name.clone()).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        // }
+        // if !scores.contains_key(&team_2_name){
+        //     scores.entry(team_2_name.clone()).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        // }
+        
+        scores.entry(team_1_name).and_modify(|t|{t.goals_scored+=team_1_score;t.goals_conceded+=team_2_score;}).or_insert(Team{goals_scored:team_1_score,goals_conceded:team_2_score});
+        scores.entry(team_2_name).and_modify(|t|{t.goals_scored+=team_2_score;t.goals_conceded+=team_1_score;}).or_insert(Team{goals_scored:team_2_score,goals_conceded:team_1_score});
+        
+        // score1.unwrap().goals_scored+=team_1_score;
+        // score1.unwrap().goals_conceded+=team_2_score;
+        
+
     }
     scores
 }
