677aec465871593a09551f3a8e5d9ccd4a5a45f0
YYmicro <YYmicro1044566658@outlook.com>
2024-09-19 14:28:23 +0800

    string

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let tr = input.trim().to_string();
+    tr
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let tr = input.to_string()+" world!";
+    // let tr = input.to_string().
+    tr
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let tr = input.replace("cars", "balloons");
+    tr
 }
 
 #[cfg(test)]
